0414425bb3f8a1de0ba109898fdc5ddd241cb75b
kapicaluk <kapicaluk@gmail.com>
2017-09-27 12:51:52 -0400

    internalResolve && internalReject

--- src/pledge.js
+++ src/pledge.js
@@ -3,21 +3,28 @@
 Promises Workshop: build the pledge.js ES6-style promise library
 ----------------------------------------------------------------*/
 // YOUR CODE HERE:
-function $Promise(executor){
-    if(typeof executor !== 'function') throw new TypeError('/executor.+function/i');
+function $Promise(executor) {
+    if (typeof executor !== 'function') throw new TypeError('/executor.+function/i');
 
     this._state = 'pending';
     this._value = {};
 }
 
-$Promise.prototype._internalResolve = function(data){
-    if(this._state === 'pending') this._value = data;
-    this._state = 'fulfilled';
+$Promise.prototype._internalResolve = function(data) {
+    if (this._state !== 'rejected' && this._state === 'pending') {
+        this._value = data;
+        this._state = 'fulfilled';
+    }
 };
 
-$Promise.prototype._internalReject = function(){
-
+$Promise.prototype._internalReject = function(reason) {
+    if (this._state !== 'fulfilled' && this._state === 'pending') {
+        this._value = reason;
+        this._state = 'rejected';
+    }
 };
+
+
 
 
 
