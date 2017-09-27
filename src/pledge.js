14115cd3f3ac87d6c672f7400e5bbe3979fa7d38
Gabriel Defazio <gabrieldefazio@Gabriels-MacBook-Air.local>
2017-09-27 18:24:46 -0400

    after review

--- src/pledge.js
+++ src/pledge.js
@@ -3,6 +3,12 @@
 Promises Workshop: build the pledge.js ES6-style promise library
 ----------------------------------------------------------------*/
 // YOUR CODE HERE:
+// function isPending(state){
+//     return this._state = 'pending'
+// }
+function isFn(thing){
+    return typeof thing === "function";
+}
 function $Promise(executor) {
     if (typeof executor !== 'function') throw new TypeError('/executor.+function/i');
 
@@ -13,27 +19,43 @@ function $Promise(executor) {
     executor(this._internalResolve.bind(this), this._internalReject.bind(this));
 }
 
-$Promise.prototype._internalResolve = function(data) {
-    if (this._state !== 'rejected' && this._state === 'pending') {
-        this._value = data;
-        this._state = 'fulfilled';
-    }
+$Promise.prototype._settle =function(newState, newValue){
+    if (this._state === 'pending') {
+        this._state = newState;
+        this._value = newValue;
+    };
+    this._callHandlers();
+}
 
+
+$Promise.prototype._internalResolve = function(data) {
+    this._settle('fulfilled', data)
 };
 
 $Promise.prototype._internalReject = function(reason) {
-    if (this._state !== 'fulfilled' && this._state === 'pending') {
-        this._value = reason;
-        this._state = 'rejected';
-    }
+    this._settle('rejected', reason)
 };
 
-$Promise.prototype.then = function(success, error){
-    if(typeof success === 'function' && typeof error === 'function'){
-        this._handlerGroups.push({ successCb: success, errorCb: error })
-    }else this._handlerGroups.push({ successCb: undefined, errorCb: undefined })
+$Promise.prototype.then = function(successCb, errorCb) {
+    this._handlerGroups.push({
+        successCb: isFn(successCb) ? successCb : null ,
+        errorCb: isFn(errorCb) ? errorCb : null  });
+
+    this._callHandlers();
 };
 
+$Promise.prototype._callHandlers = function(){
+    if(this._state === 'pending') return;
+    let cbType = this._state === 'fulfilled' ? 'successCb' : 'errorCb';
+    this._handlerGroups.forEach(handlerGroup=>{
+        if(handlerGroup[cbType]) handlerGroup[cbType](this._value);
+    });
+    this._handlerGroups = [];
+};
+
+$Promise.prototype.catch = function (errorCb) {
+    this.then(null, errorCB)
+};
 
 
 
